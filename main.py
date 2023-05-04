0138b5c20502ffe5ce6174dd45fba77bb9c44427
rmEleven <1569323975@qq.com>
2023-05-04 22:54:26 +0800

    修改导入模块

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from Window import *
 
 
 if __name__ == '__main__':
-
     app = QApplication(sys.argv)  # 创建一个 QApplication 对象
     myWindow = MainWindow()       # 创建一个 MainWindow 对象
     sys.exit(app.exec_())         # 开始应用程序的主循环
