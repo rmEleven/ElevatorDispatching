8b7582c907a3c0931c7c6719615273f8e276830d
rmEleven <1569323975@qq.com>
2023-05-04 15:34:30 +0800

    完成电梯类的设计

--- Elevator.py
+++ Elevator.py
@@ -7,9 +7,7 @@ import time
 from functools import partial
 
 class Elevator(QThread):
-    '''
-    创建一个名为Elevator的类，继承QThread类
-    '''
+    '''创建一个名为Elevator的类模拟电梯，继承QThread类'''
 
     trigger = pyqtSignal(int)  # 实例化一个信号对象
 
@@ -18,86 +16,97 @@ class Elevator(QThread):
         super().__init__()                # 调用父类QThread的构造函数
         self.trigger.connect(self.check)  # 将信号trigger连接到check函数
 
-        self.id = id                  # 电梯编号id
-        self.mainWindow = mainWindow  # 电梯图形化界面
+        self.id = id                      # 电梯编号id
+        self.mainWindow = mainWindow      # 电梯图形化界面
 
-        self.arrive = 0         # 电梯是否到达目标楼层
-        self.pause = 0          # 电梯是否暂停运行
-        self.state = 0          # 电梯状态 0:停止 1:向上运行 -1:向下运行
-        self.floor = 1          # 电梯当前所在楼层
-        self.target = set([])   # 电梯的目标楼层集合
+        self.arrive = 0                   # 电梯是否到达目标楼层
+        self.error = 0                    # 电梯是否出现故障
+        self.state = 0                    # 电梯状态 0:停止 1:向上运行 -1:向下运行
+        self.floor = 1                    # 电梯当前所在楼层
+        self.target = set([])             # 电梯的目标楼层集合
 
     def run(self):
         '''重写QThread的run方法'''
         while (1):
             if self.arrive == 1:  # 电梯到达目标楼层
-                # 设置按钮的样式为开门图片
-                self.mainWindow.findChild(QPushButton, "open{0}".format(self.id)).setStyleSheet("QPushButton{background-image: url(open.png)}")
-                time.sleep(3)  # 线程休眠3秒
-                # 将按钮的样式设置为空
-                self.mainWindow.findChild(QPushButton, "open{0}".format(self.id)).setStyleSheet("QPushButton{}")
-                self.arrive = 0  # 将arrive设置为0
+                self.mainWindow.findChild(QPushButton, "open{0}".format(self.id)).setStyleSheet("QPushButton{background-image: url(open.png)}")  # 设置按钮的样式为开门图片
+                time.sleep(3)     # 线程休眠3秒
+                self.mainWindow.findChild(QPushButton, "open{0}".format(self.id)).setStyleSheet("QPushButton{}")  # 将按钮的样式设置为空
+                self.arrive = 0   # 将arrive设置为0
             
-            self.trigger.emit(self.id)  # 发射信号trigger，并传递参数self.id
+            self.trigger.emit(self.id)  # 发射trigger信号 调用check函数
             time.sleep(1)               # 线程休眠1秒
     
     def check(self):
-        if self.pause == 0:  # 电梯没有暂停运行
+        '''检查电梯是否到达目标楼层并调整其状态'''
+
+        '''故障电梯'''
+        if self.error == 1:  # 电梯出现故障
+            return           # 不做处理
             
-            '''改变电梯楼层'''
-            #if (self.floor in self.target) or (self.floor in self.mainWindow.up) or (self.floor in self.mainWindow.down):
+        '''改变电梯楼层'''
+        if (self.floor not in self.target) and (self.floor not in self.mainWindow.up) and (self.floor not in self.mainWindow.down):
             if self.state == 1:              # 电梯处于向上状态
                 self.floor = self.floor + 1  # 向上移动一层
             elif self.state == -1:           # 电梯处于向下状态
                 self.floor = self.floor - 1  # 向下移动一层
-            self.mainWindow.findChild(QLCDNumber, "{0}".format(self.id)).display(self.floor)  # 显示电梯楼层
+        self.mainWindow.findChild(QLCDNumber, "{0}".format(self.id)).display(self.floor)  # 显示电梯楼层
+
+        '''电梯处于向上状态'''
+        if self.state == 1:
+            self.mainWindow.findChild(QPushButton, "{0}+{1}".format(self.id, self.floor)).setStyleSheet("QPushButton{}")  # 去掉电梯内部按钮标识
+            self.mainWindow.findChild(QPushButton, "up{0}".format(self.floor)).setStyleSheet("QPushButton{}")             # 去掉电梯外部按钮标识
+            if (self.floor in self.target) or (self.floor in self.mainWindow.up):  # 到达目标楼层
+                self.arrive = 1
+            self.target.discard(self.floor)         # 从目标楼层集合中删除该层
+            self.mainWindow.up.discard(self.floor)  # 从楼层上升集合中删除该层
+
+            if len(list(self.target)) == 0:            # 目标楼层集合为空
+                self.state = 0                         # 电梯进入静止状态
+            elif max(list(self.target)) < self.floor:  # 最大目标楼层小于当前楼层
+                self.state = -1                        # 电梯向下移动
+
+        '''电梯处于向下状态'''
+        if self.state == -1:
+            self.mainWindow.findChild(QPushButton, "{0}+{1}".format(self.id, self.floor)).setStyleSheet("QPushButton{}")  # 去掉电梯内部按钮标识
+            self.mainWindow.findChild(QPushButton, "down{0}".format(self.floor)).setStyleSheet("QPushButton{}")           # 去掉电梯外部按钮标识
+            if (self.floor in self.target) or (self.floor in self.mainWindow.down):  # 到达目标楼层
+                self.arrive = 1
+            self.target.discard(self.floor)           # 从目标楼层集合中删除该层
+            self.mainWindow.down.discard(self.floor)  # 从楼层下降集合中删除该层
+
+            if len(list(self.target)) == 0:            # 目标楼层集合为空
+                self.state = 0                         # 电梯进入静止状态
+            elif min(list(self.target)) > self.floor:  # 最小目标楼层大于当前楼层
+                self.state = 1                         # 电梯向上移动
+
+        '''电梯处于静止状态'''            
+        if self.state == 0:
+            self.mainWindow.findChild(QPushButton, "{0}+{1}".format(self.id, self.floor)).setStyleSheet("QPushButton{}")  # 去掉电梯内部按钮标识
+            self.mainWindow.findChild(QPushButton, "up{0}".format(self.floor)).setStyleSheet("QPushButton{}")             # 去掉电梯外部按钮标识
+            self.mainWindow.findChild(QPushButton, "down{0}".format(self.floor)).setStyleSheet("QPushButton{}")           # 去掉电梯外部按钮标识
             
-            '''电梯处于向上状态'''
-            if self.state == 1:
-                self.mainWindow.findChild(QPushButton, "{0}+{1}".format(self.id, self.floor)).setStyleSheet("QPushButton{}")  # 去掉电梯内部该层的标识
-                self.mainWindow.findChild(QPushButton, "up{0}".format(self.floor)).setStyleSheet("QPushButton{}")             # 去掉电梯外部该层的标识
-                if (self.floor in self.target) or (self.floor in self.mainWindow.up):  # 到达目标楼层
-                    self.arrive = 1
-                self.target.discard(self.floor)         # 从目标楼层集合中移除该层
-                self.mainWindow.up.discard(self.floor)  # 从楼层等待集合中移除该层
-
-                if len(list(self.target)) == 0:  # 电梯要到达的目标楼层集合为空
-                    self.state = 0               # 电梯已经没有任务，状态置为0，表示电梯静止等待。
-                # 如果电梯要到达的目标楼层列表不为空，且列表中最大的目标楼层小于当前楼层，说明电梯应该向下运动，状态置为-1，表示电梯正在向上运动。
-                if (len(list(self.target)) != 0) and (max(list(self.target)) < self.floor):
-                    self.state = -1
-
-            '''电梯处于向下状态'''
-            if self.state == -1:
-                self.mainWindow.findChild(QPushButton, "{0}+{1}".format(self.id, self.floor)).setStyleSheet("QPushButton{}")  # 去掉电梯内部该层的标识
-                self.mainWindow.findChild(QPushButton, "down{0}".format(self.floor)).setStyleSheet("QPushButton{}")           # 去掉电梯外部该层的标识
-                if (self.floor in self.target) or (self.floor in self.mainWindow.down):  # 到达目标楼层
-                    self.arrive = 1
-                self.target.discard(self.floor)           # 从目标楼层集合中移除该层
-                self.mainWindow.down.discard(self.floor)  # 从楼层等待集合中移除该层
-
-                if len(list(self.target)) == 0:  # 电梯要到达的目标楼层集合为空
-                    self.state = 0               # 电梯已经没有任务，状态置为0，表示电梯静止等待。
-                # 如果电梯要到达的目标楼层列表不为空，且列表中最小的目标楼层大于当前楼层，说明电梯应该向上运动，状态置为1，表示电梯正在向上运动。
-                if (len(list(self.target)) != 0) and (min(list(self.target)) > self.floor):
-                    self.state = 1
-
-            '''电梯处于静止状态'''            
-            if self.state == 0:
-                # 如果电梯内有要到达的楼层并且电梯内的目标楼层最大值大于当前电梯所在的楼层
-                if (len(list(self.target)) != 0) and (max(list(self.target)) > self.floor):
-                    self.state = 1  # 向上运动
-                # 如果电梯内有要到达的楼层并且电梯内的目标楼层最小值小于当前电梯所在的楼层
-                if (len(list(self.target)) != 0) and (min(list(self.target)) < self.floor):
-                    self.state = -1  # 向下运动
+            if (self.floor in self.target) or (self.floor in self.mainWindow.up) or (self.floor in self.mainWindow.down):  # 到达目标楼层
+                self.arrive = 1
+            self.target.discard(self.floor)           # 从目标楼层集合中删除该层
+            self.mainWindow.up.discard(self.floor)    # 从楼层上升集合中删除该层
+            self.mainWindow.down.discard(self.floor)  # 从楼层下降集合中删除该层
+            
+            if len(list(self.target)) == 0:            # 目标楼层集合为空
+                self.state = 0                         # 电梯进入静止状态
+            elif max(list(self.target)) > self.floor:  # 最大目标楼层大于当前楼层
+                self.state = 1                         # 电梯向上移动
+            elif min(list(self.target)) < self.floor:  # 最小目标楼层小于当前楼层
+                self.state = -1  # 电梯向上移动
         
-    def pause(self):
-        if self.pause == 0:  # 如果该电梯未被暂停
-            self.pause = 1  # 将该电梯的暂停状态设为已暂停
-            self.mainWindow.findChild(QPushButton, "pause{0}".format(self.id)).setText("暂停")  # 将该电梯对应的暂停按钮文本设置为“暂停”
-        else:  # 如果该电梯已被暂停
-            self.pause = 0  # 将该电梯的暂停状态设为未暂停
-            self.mainWindow.findChild(QPushButton, "pause{0}".format(self.id)).setText("启动")  # 将该电梯对应的暂停按钮文本设置为“启动”
+    def error(self):
+        '''改变电梯故障状态'''
+        if self.error == 0:  # 电梯没有故障
+            self.error = 1   # 设置为出现故障
+            self.mainWindow.findChild(QPushButton, "error{0}".format(self.id)).setText("出现故障")  # 设置按钮文本
+        else:                # 电梯出现故障
+            self.error = 0   # 设置为没有故障
+            self.mainWindow.findChild(QPushButton, "error{0}".format(self.id)).setText("正常运行")  # 设置按钮文本
 
     def add_target(self, floor):
         '''将楼层添加到电梯的目标楼层集合中'''
